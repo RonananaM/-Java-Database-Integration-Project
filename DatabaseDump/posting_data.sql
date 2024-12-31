2c00d56e66523f99876e1a61be759c8e66d46223
Rona <148178428+RonananaM@users.noreply.github.com>
2024-12-31 12:43:50 +0800

    Create posting_data.sql

---
+++ DatabaseDump/posting_data.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM job_tracking.posting_data
+LIMIT 0, 1000
+
+-- Date: 2018-04-04 02:09
+*/
+INSERT INTO `posting_data` (`posting_id`,`posting_type`,`creator_id`,`job_title`,`job_state`,`company_name`,`contact_email`,`major`,`classification`,`req_experience`,`salary`) VALUES (100001,'INTERNSHIP','100013','Coffee Fetcher','NA','Bio Side','hiring@bioside.com','Culinary Arts','Senior',0,0);
+INSERT INTO `posting_data` (`posting_id`,`posting_type`,`creator_id`,`job_title`,`job_state`,`company_name`,`contact_email`,`major`,`classification`,`req_experience`,`salary`) VALUES (100002,'REGULAR','100013','Phone Cleaner','NA','Bio Side','HR@BioCide.com',' ',' ',21,29001);
+INSERT INTO `posting_data` (`posting_id`,`posting_type`,`creator_id`,`job_title`,`job_state`,`company_name`,`contact_email`,`major`,`classification`,`req_experience`,`salary`) VALUES (100003,'INTERNSHIP','100012','Code Thrasher','NA','Microsoft','HR@microsoft.com','Computer Science','Junior',0,0);
+INSERT INTO `posting_data` (`posting_id`,`posting_type`,`creator_id`,`job_title`,`job_state`,`company_name`,`contact_email`,`major`,`classification`,`req_experience`,`salary`) VALUES (100004,'REGULAR','100007','Greeter','NA','Mega Mart','HR@megamart.com',' ',' ',17,9000);
+INSERT INTO `posting_data` (`posting_id`,`posting_type`,`creator_id`,`job_title`,`job_state`,`company_name`,`contact_email`,`major`,`classification`,`req_experience`,`salary`) VALUES (100008,'INTERNSHIP','100007','Junior Apprentice','NA','Wizards R Us','HR@Magic.Net','Bachelors of MagicArts','Senior',0,0);
